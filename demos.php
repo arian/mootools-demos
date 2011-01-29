d16afc1ac7651f59b004a367870c29fbdbdf2982
Arian <arian@aryweb.nl>
2011-01-29 01:28:39 +0100

    Little work on the demos page

--- demos.php
+++ demos.php
@@ -1,8 +1,31 @@
 <?php if (substr($_SERVER['SCRIPT_NAME'], -9) == 'demos.php') die(); ?>
 <h1>Even More Demos!</h1>
 
+<p>
+	This is a collection of demos that is mostly to support our documentation with
+	real working examples. That is why you shouldn't expect that they are as fancy
+	as the others, however they are useful as example if you'd like to use one of
+	the components provided by MooTools.
+</p>
+
 <div class="demoslist">
 
+	<div>
 
+		<h2>MooTools More</h2>
+
+
+		<ul>
+			<li><a href="?demo=Element.Pin">Element.Pin</a></li>
+		</ul>
+
+	</div>
+
+
+	<div>
+
+		<h2>ART</h2>
+
+	</div>
 
 </div>
