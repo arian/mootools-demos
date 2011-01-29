48747ef719202c8893fefd684ddc2ac8c37ba1e4
Arian <arian@aryweb.nl>
2011-01-29 01:27:55 +0100

    Improve the Element.Pin demo a little

--- demos/Element.Pin/demo.js
+++ demos/Element.Pin/demo.js
@@ -1,17 +1,20 @@
 window.addEvent('domready', function(){
+
 	var pinner = document.id('pinner');
-	document.id('pin').addEvent('click', function(e) {
-		e.preventDefault();
+
+	document.id('pin').addEvent('click', function(event){
+		event.stop();
 		pinner.pin();
 	});
-	
-	document.id('unpin').addEvent('click', function(e) {
-		e.preventDefault();
+
+	document.id('unpin').addEvent('click', function(event){
+		event.stop();
 		pinner.unpin();
 	});
-	
-	document.id('toggle').addEvent('click', function(e) {
-		e.preventDefault();
+
+	document.id('toggle').addEvent('click', function(event){
+		event.stop();
 		pinner.togglepin();
 	});
+
 });
